ff13d4c0a2a9bd48707f8b08f0175876bfd39bfb
kolbesurran <kolbe@deuceslax.com>
2019-10-11 09:59:18 -0500

    fixed the error with the second view controller

---
+++ Recycle/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  Recycle
+//
+//  Created by Kolbe Surran on 10/11/19.
+//  Copyright © 2019 Shockedeel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var mainView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
